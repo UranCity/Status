d34141aa5b9c5aaf7aa5bf56a6edf0794963c612
Valentin_Kh <72650892+ValentinKhmer@users.noreply.github.com>
2022-03-21 22:47:32 +0100

    Backup worker.js with tiny modifications

--- worker.js
+++ worker.js
@@ -1,20 +1,16 @@
-async function checkStatus() {
-	await fetch("https://api.mcsrvstat.us/simple/mc.urancity.ml").then((res) => {
-		if (res.status === 200 || res.status === 404) {
-			return res.status;
-		} else {
-			console.warn(`Mauvais status renvoyé (${res.status}) (mcsrvstat.us)`);
-			return undefined;
-		}
-	});
-}
-
 async function handleRequest(request) {
+	let status = undefined;
+
+	await fetch("https://api.mcsrvstat.us/simple/mc.urancity.ml").then((res) => {
+		if (res.status === 200 || res.status === 404) status = res.status;
+		else console.warn(`Mauvais status renvoyé (${res.status}) (mcsrvstat.us)`);
+	});
+
 	return Response.redirect(
-		`https://raw.githubusercontent.com/UranCity/Status/main/assets/${checkStatus()}.png`,
+		`https://raw.githubusercontent.com/UranCity/Status/main/assets/${status}.png`,
 		303
 	);
-}
+};
 
 addEventListener("fetch", async (event) => {
 	event.respondWith(handleRequest(event.request));
